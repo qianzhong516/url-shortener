dee240a4ccfa4d4443c9f6531922764e449cebf5
qianzhong516 <zhongqian516@gmail.com>
2024-06-10 16:53:38 +0200

    set up global theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      cyan: '#2acfcf',
+      red: '#f46262',
+      gray: '#bfbfbf',
+      white: '#ffffff',
+      black: '#000000',
+    },
     extend: {
+      colors: {
+        'dark-violet': '#3b3054',
+        'darker-violet': '#232127',
+        'gray-violet': '#9e9aa7',
+        'dark-blue': '#35323e',
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial':
+          'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
