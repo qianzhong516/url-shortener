cc35ebbbbab9d9c859111b17b474c6154ed48efc
qianzhong516 <zhongqian516@gmail.com>
2024-06-18 17:06:17 +0200

    update navigation text styles

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function heroSection() {
     return <section className='flex flex-wrap items-center px-12 pt-24 pb-36 gap-20 max-w-lg'>
         <div className='flex-1'>
-            <h1 className='text-7xl font-bold'>More than just shorter links</h1>
+            <h1 className='text-7xl font-bold mb-4'>More than just shorter links</h1>
             <p className='text-xl text-gray-violet'>Build your brand&apos;s recognition and get detailed insights on how your links are performing.</p>
             <Button className='text-xl mt-10 px-8 py-4'>Get Started</Button>
         </div>
--- app/ui/navigation/nav_links.tsx
+++ app/ui/navigation/nav_links.tsx
@@ -8,6 +8,6 @@ const navLinks = [
 
 export default function NavLinks() {
     return <div className="flex gap-5">
-        {navLinks.map(({ text, href }, i) => <Link key={i} href={href}>{text}</Link>)}
+        {navLinks.map(({ text, href }, i) => <Link key={i} href={href} className='font-semibold text-gray hover:text-black'>{text}</Link>)}
     </div>
 }
